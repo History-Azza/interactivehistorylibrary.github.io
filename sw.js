cac4a3077c7e93c1c4a624842978714b390dcafc
History-Azza <azzaabaza65@gmail.com>
2026-08-26 23:37:38 +0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "smart-braille-historian-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./Smart_Braille_Historian_Prototype.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
